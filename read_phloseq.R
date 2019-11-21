f0832a70dd63ed0af7e24cf2933b1a538de2364d
Carley <ccarley@iastate.edu>
2019-11-21 11:52:00 -0600

    delete duplicate read_ and add BiocManager code

--- read_phloseq.R
+++
@@ -1,4 +0,0 @@
-setwd("./Apples/") #set this to the location you downloaded the apples.RDS file. Otherwise this won't work!
-library(phyloseq)
-
-phy <- readRDS("apples.RDS")
--- read_phyloseq.R
+++ read_phyloseq.R
@@ -1,3 +1,13 @@
+
+source('http://bioconductor.org/biocLite.R')
+biocLite('phyloseq')
+
+#If bioconductor wont work in your environment you can use BiocManager as shown below to instal the phyloseq package
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(c("phyloseq"))
+
+
 setwd("./Apples/") #set this to the location you downloaded the apples.RDS file. Otherwise this won't work!
 library(phyloseq)
 
